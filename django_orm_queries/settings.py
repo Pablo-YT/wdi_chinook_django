4441b8a2609474eea9da91105225cfd9d3df5b16
Josh Date <jjdate@live.com>
2019-03-05 17:13:53 -0500

    still working on questions

--- django_orm_queries/settings.py
+++ django_orm_queries/settings.py
@@ -78,6 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'chinook',
+        'USER': 'joshd',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
